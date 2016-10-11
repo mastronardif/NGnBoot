b47e98749b8d3af91de52553eda0f497e9bbdae4
Frank Mastronardi <mastronardif@gmail.com>
2016-10-10 22:44:39 -0400

    add a web job to app data

---
+++ WebApplication3/App_Data/jobs/triggered/cleantempimages/run.ps1
@@ -0,0 +1,6 @@
+#Write-Host "FM BEGIN" 
+$env:APPSETTING_IntroMessage
+#Write-Output "write to output"
+#[console]::WriteLine "what the f"
+#Write-Verbose "Verbose outside of function" -Verbose
+#Write-Host "FM END" 
