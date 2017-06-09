00efcb3f9100cc41a63011cfb0c2ae1a04236922
mastronardif <mastronardif@gmail.com>
2017-06-09 16:53:42 -0400

    using html helpers

--- WebApplication3/Models/HomeMyvmModels.cs
+++ WebApplication3/Models/HomeMyvmModels.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 
 namespace WebApplication3.Models
 {
+    public class Employee {
+        public string   name { get; set; }
+        public int       age { get; set; }
+        public double salary { get; set; }
+    }
+
     public class MyViewModel
     {
         public string SelectedSexPreference { get; set; }
         public Dictionary<string, string> SexPreferences { get; set; }
 
+        public Dictionary<int, Employee> employees { get; set; }
+
         public MyViewModel()
         {
             SexPreferences = new Dictionary<string, string>();
+            LoadEmployees();
         }
+
+        // setup empoloyees
+        private void LoadEmployees()
+        {
+            employees = new Dictionary<int, Employee>() {
+                { 1001, new Employee{ name="Bob",  age=33, salary=123.123}},
+                { 1002, new Employee{ name="Fred", age=33, salary=124.123}},
+                { 1003, new Employee{ name="Ann",  age=33, salary=125.123}}
+            };
+
+
+        }
+
     }
 }
