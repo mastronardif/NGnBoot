dac58c2f82c1a32d921fa350b0bd0a80b114626b
mastronardif <mastronardif@gmail.com>
2017-06-14 16:18:11 -0400

    house cleaning

---
+++ WebApplication3/Scripts/my/controlers/mycontroller01.js
@@ -0,0 +1,27 @@
+﻿'use strict'
+myapp.controller("myappCtrl", function ($scope, $http) {
+
+        $scope.name = "Bary NewBonds";
+       
+        $scope.ads = "";
+
+        /***/
+        var url = '/Home/GetAds/';
+        $http.get(url)
+            .then(function (resp) {
+                $scope.ads = resp.data;
+                console.log("ads = ", $scope.ads);
+            });
+        /*******/
+
+        $scope.save = function () {
+            alert('scope.save()');
+        },
+
+        $scope.init = function (employees) {
+            $scope.employees = employees;
+            console.log('INIT INIT');
+        }
+
+
+    });
